ba8893021690b0ef9b54ee83fc35375fe4d75c24
EBrain <ebrain.ebrain@gmail.com>
2023-03-06 21:22:25 +0630

    Rename label into badge in css

--- src/components/MissionsBadge.js
+++ src/components/MissionsBadge.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from 'src/css/MissionsLabel.module.css';
+import styles from 'src/css/MissionsBadge.module.css';
 
 function MissionsBadge({ isReserved }) {
   return (

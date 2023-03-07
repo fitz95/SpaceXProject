49eb465ee44f568f7a16544644ac1028e7ad4107
fitz95 <fitzgeraldnsong@gmail.com>
2023-03-07 12:20:01 +0100

    add the selected rockets to Profile Page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,11 @@ import { useSelector } from 'react-redux';
 
 import styles from 'src/css/Profile.module.css';
 import { selectReservedMissions } from 'src/redux/missionsSlice';
+import { selectReservedRockets } from 'src/redux/rocketsSlice';
 
 function Profile() {
   const reservedMissions = useSelector(selectReservedMissions);
+  const reservedRockets = useSelector(selectReservedRockets);
 
   return (
     <main className={styles.main}>
@@ -15,6 +17,16 @@ function Profile() {
           {reservedMissions.map((e) => (
             <li key={e.mission_id} className={styles.listItem}>
               {e.mission_name}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className={styles.section}>
+        <h2 className={styles.title}>My Rockets</h2>
+        <ul className={styles.list}>
+          {reservedRockets.map((e) => (
+            <li key={e.rocket_id} className={styles.listItem}>
+              {e.rocket_name}
             </li>
           ))}
         </ul>

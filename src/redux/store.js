1e8e8c83c5a0116086f13e907673dd6fa333a73f
EBrain <ebrain.ebrain@gmail.com>
2023-03-07 19:02:07 +0630

    Add logger for redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,19 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
 
 import missionsReducer from './missionsSlice';
 import rocketsReducer from './rocketsSlice';
+
+const logger = createLogger({
+  collapsed: true,
+});
 
 const store = configureStore({
   reducer: combineReducers({
     missions: missionsReducer,
     rockets: rocketsReducer,
   }),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;

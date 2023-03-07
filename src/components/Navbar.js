6be2319cd3b3aba4cfe5a8d9c159477e544c2563
fitz95 <fitzgeraldnsong@gmail.com>
2023-03-07 10:31:28 +0100

    styling the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 import styles from 'src/css/Navbar.module.css';
+import logo from '../image/logo.png';
 
 function Navbar() {
   return (
     <header className={styles.header}>
-      <NavLink to="/">Space Travelers&apos; Hub</NavLink>
+      <NavLink to="/" className={styles.icondiv}>
+        <img src={logo} alt="planet" className={styles.icon} />
+        <span className={styles.navspan}>Space Travelers&apos; Hub</span>
+      </NavLink>
       <nav>
         <ul className={styles.navItems}>
           <li>
-            <NavLink to="/">Rockets</NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Rockets
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/missions">Missions</NavLink>
+            <NavLink
+              to="/missions"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Missions
+            </NavLink>
           </li>
+          <hr />
           <li>
-            <NavLink to="/my-profile">My Profile</NavLink>
+            <NavLink
+              to="/my-profile"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              My Profile
+            </NavLink>
           </li>
         </ul>
       </nav>
